d9f3b86a424070f3d3daf0ee711940196c52f410
Jammed-Up <joshmeyer.mba@gmail.com>
2026-05-14 13:06:40 -0500

    fix: export ClientPrincipal interface

--- 1on1-app/src/lib/auth.ts
+++ 1on1-app/src/lib/auth.ts
@@ -1,6 +1,6 @@
 import { NextRequest } from "next/server";
 
-interface ClientPrincipal {
+export interface ClientPrincipal {
   identityProvider?: string;
   userId: string;
   userDetails: string;
